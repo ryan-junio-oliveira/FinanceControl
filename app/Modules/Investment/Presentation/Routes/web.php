ec1d5a3cb25f533fb728693dab3ea68d31c12b1c
ryan-junio-oliveira <ryan.oliveira.personal@outlook.com>
2026-02-22 18:40:06 -0300

    fix(investments): include web middleware on routes

--- app/Modules/Investment/Presentation/Routes/web.php
+++ app/Modules/Investment/Presentation/Routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Modules\Investment\Presentation\Http\Controllers\InvestmentController;
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['web','auth','force_password_change'])->group(function () {
     Route::get('/investments', [InvestmentController::class, 'index'])->name('investments.index');
     Route::get('/investments/create', [InvestmentController::class, 'create'])->name('investments.create');
     Route::post('/investments', [InvestmentController::class, 'store'])->name('investments.store');
